13097b996339e98c8c6b3519c57994519e1f52a1
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-02 17:51:07 +0530

    refactor: reconfigure codebase according to proper conventions

--- app.js
+++ app.js
@@ -4,29 +4,27 @@ require('dotenv').config();
 
 const { urlencoded, json } = require('body-parser');
 const request = require('request');
+const configHolder = require('./config');
 
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.use(urlencoded({ extended: true }));
 app.use(json());
-
-const port = process.env.PORT || 8080;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 app.get('/', (req, res) => {
     res.send('Working...');
 });
 
 app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
     if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        if (mode === 'subscribe' && token === configHolder.VERIFY_TOKEN) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
         } else {
@@ -39,7 +37,7 @@ app.post('/webhook', (req, res) => {
     let body = req.body;
 
     if (body.object === 'page') {
-        body.entry.forEach(function(entry) {
+        body.entry.forEach((entry) => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
@@ -64,7 +62,7 @@ app.listen(port, () => {
 });
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
+const handleMessage = (sender_psid, received_message) => {
     let response;
 
     // Checks if the message contains text
@@ -72,30 +70,30 @@ function handleMessage(sender_psid, received_message) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+            'text': `You sent the message: '${received_message.text}'. Now send me an attachment!`
         }
     } else if (received_message.attachments) {
         // Get the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
         response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [{
+                        'title': 'Is this the right picture?',
+                        'subtitle': 'Tap a button to answer.',
+                        'image_url': attachment_url,
+                        'buttons': [
                             {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
+                                'type': 'postback',
+                                'title': 'Yes!',
+                                'payload': 'yes',
                             },
                             {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
+                                'type': 'postback',
+                                'title': 'No!',
+                                'payload': 'no',
                             }
                         ],
                     }]
@@ -109,7 +107,7 @@ function handleMessage(sender_psid, received_message) {
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+const handlePostback = (sender_psid, received_postback) => {
     let response;
 
     // Get the payload for the postback
@@ -117,35 +115,35 @@ function handlePostback(sender_psid, received_postback) {
 
     // Set the response based on the postback payload
     if (payload === 'yes') {
-        response = { "text": "Thanks!" }
+        response = { 'text': 'Thanks!' }
     } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
+        response = { 'text': 'Oops, try sending another image.' }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
+const callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
-        "recipient": {
-            "id": sender_psid
+        'recipient': {
+            'id': sender_psid
         },
-        "message": response
+        'message': response
     }
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': { 'access_token': configHolder.ACCESS_TOKEN },
+        'method': 'POST',
+        'json': request_body
     }, (err, res, body) => {
         if (!err) {
             console.log('message sent!')
         } else {
-            console.error("Unable to send message:" + err);
+            console.error('Unable to send message:' + err);
         }
     });
 }
---
+++ config.js
@@ -0,0 +1,7 @@
+const configHolders = {
+    VERIFY_TOKEN: process.env.VERIFY_TOKEN,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+    APP_ID: process.env.APP_ID
+};
+
+module.exports = configHolders;
