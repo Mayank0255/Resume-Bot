f22b22ecc73b5471776f2383b489ffa86b03cc6f
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-02 13:44:58 +0530

    feat: setup the webhook

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const {
-    urlencoded,
-    json
-} = require('body-parser');
+'use strict';
+
+const { urlencoded, json } = require('body-parser');
 
 
 const express = require('express');
@@ -10,7 +9,39 @@ const app = express();
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-port = process.env.PORT || 8080;
+app.get('/webhook', (req, res) => {
+    let VERIFY_TOKEN = "6YQo3ntcovUH7cRZ29ijNdO5XMxYxlU8oc1069FbNj0="
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+
+    if (body.object === 'page') {
+        body.entry.forEach(function(entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Running on ${port}`);
