540618a66dd0af0c014cfbc558206734a3e76e16
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-07 20:31:17 +0530

    feat: projects section implementation

---
+++ resources/projects.js
@@ -0,0 +1,28 @@
+const projectsSection = `
+    \\section{Projects}
+
+    %% Another custom command provide by scimisc-cv.sty.
+    %% First two argumetns are typeset on the first line in bold; 3rd and 4th arguments are typset on second line in italics. 2nd, 3rd and 4th arguments are OPTIONAL
+    `
+
+const setTitleAndLink = (title, link) => {
+    return `
+        \\cvsubsection{${title}}[\\href{${link}}{Click Here}]`;
+}
+
+const setTimeline = (timeline) => {
+    return `
+        [${timeline}][]`;
+}
+
+const setDescription = (description) => {
+    return `
+        ${description}`;
+}
+
+module.exports = {
+    projectsSection,
+    setTitleAndLink,
+    setTimeline,
+    setDescription
+}
--- services/resumeConstruction.js
+++ services/resumeConstruction.js
@@ -5,15 +5,18 @@ const { setName, setEmail, setPhone, setLinkedin, setPortfolio } = require('../r
 const { educationSection, setInstituteAndLocation, setMajor, setMarks, setStream } = require('../resources/education');
 const { skillsSection, setSkills } = require('../resources/skills');
 const { experienceSection, setRoleAndLocation, setCompanyAndTimeline, setDesc } = require('../resources/experience');
+const { projectsSection, setTitleAndLink, setTimeline, setDescription } = require('../resources/projects');
 
 let educationPush = true;
 let skillsPush = true;
 let experiencePush = true;
+let projectsPush = true;
 
 let institute = '';
 let subSkill = '';
 let company = '';
 let role = '';
+let title = '';
 
 const resumeConstruction = ( message, sectionName, question, section, folderPath ) => {
     const resume_template = fs.createWriteStream(folderPath + '/scimisc-cv.sty');
@@ -135,6 +138,36 @@ const resumeConstruction = ( message, sectionName, question, section, folderPath
                 break
             case section.questions[1].question[4].ask:
                 fs.appendFile(filePath, setDesc(message), err => {
+                    if (err) throw err;
+                });
+                break
+            default:
+                break
+        }
+    } else if (sectionName === 'projects') {
+        if (projectsPush) {
+            fs.appendFile(filePath, projectsSection, err => {
+                if (err) throw err;
+            });
+            projectsPush = false;
+        }
+
+        switch (question) {
+            case section.questions[1].question[0].ask:
+                title = message;
+                break
+            case section.questions[1].question[1].ask:
+                fs.appendFile(filePath, setTitleAndLink(title, message), err => {
+                    if (err) throw err;
+                });
+                break
+            case section.questions[1].question[2].ask:
+                fs.appendFile(filePath, setTimeline(message), err => {
+                    if (err) throw err;
+                });
+                break
+            case section.questions[1].question[3].ask:
+                fs.appendFile(filePath, setDescription(message), err => {
                     if (err) throw err;
                 });
                 break
