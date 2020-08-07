1c1907c7ddbfaa6f396c37b3219e221b73f41df2
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-07 12:42:41 +0530

    chore: ui redesign

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const configHolders = {
     VERIFY_TOKEN: process.env.VERIFY_TOKEN,
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
     APP_ID: process.env.APP_ID,
-    SEND_API: 'https://graph.facebook.com/v2.6/me/messages'
+    SEND_API: 'https://graph.facebook.com/v2.6/me/messages',
+    WEBSITE_URL: 'https://resumebuilderbot.herokuapp.com/'
 };
 
 module.exports = configHolders;
