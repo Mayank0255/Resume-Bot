33e92ccb7e9b2e74d7f9be911e30bac8ef0c6a21
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-19 00:14:14 +0530

    feat: response compressor & helmet setup

--- app.js
+++ app.js
@@ -2,32 +2,44 @@
 
 require('dotenv').config();
 
-const { urlencoded, json } = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 8080;
-const server = http.createServer(app)
-
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
+// compressing api response
+app.use(compression());
+
 // body-parser
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // logger
 app.use(morgan('dev'));
 
+// security config
+app.use(helmet());
+
+// routes
 app.use(require('./routes/webhook'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+// port initialized
+const port = process.env.PORT || 8080;
+
+// server setup
+const server = http.createServer(app)
 
 server.listen(port, () => {
     console.log(`Running on ${port}`);
